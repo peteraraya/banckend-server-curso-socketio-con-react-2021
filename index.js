638f29400fd41b2f9ca28b96afab668d16fc81e9
Pedro Araya <Piteraraya@gmail.com>
2021-06-11 23:28:16 -0400

    Se realiza refactorización de codigo, dividiendo backend en clases

--- index.js
+++ index.js
@@ -1,41 +1,9 @@
-// Servidor de express
-const express = require('express');
-const app = express();
+const Server = require("./models/server");
 
-// Servidor de sockets
-const server = require('http').createServer(app);
+const server = new Server();
 
-// Configuración del socket server
-const io = require('socket.io')(server);
-
-// Desplegar el directorio público
-app.use(express.static(__dirname + '/public'));
+server.execute();
 
 
-io.on('connection', (socket) => {
-  // console.log(socket.id);
-
-  // Emitimos evento mensaje-bienvenida
-  // socket.emit('mensaje-bienvenida', {
-  //     msg:'Bienvenido al server',
-  //     fecha: new Date()
-  // });
-
-  // Escuchamos evento mensaje-cliente
-  socket.on('mensaje-to-server', (data) => {
-    console.log(data);
-    
-    // Notificamos el mensaje que estamos recibiendo (data)
-    // socket.emit('mensaje-from-server', data);
-
-    // Envió de mensaje global con io : io emite a todos
-    io.emit('mensaje-from-server',data);
-
-  });
-
-});
 
 
-server.listen(8080, ()=>{
-  console.log('Server corriendo en puerto 8080');
-});
